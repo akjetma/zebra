2d485ccf5bf9526cae82a1ac21485b9d0b6e9990
Adam Jetmalani <adam@getlittlebird.com>
2014-06-10 15:45:38 -0700

    store things

--- client.rb
+++ client.rb
@@ -11,6 +11,34 @@ Stripe.api_version = '2014-05-19'
 
 module Zebra
   class ZebraObject
+    def self.make_or_retrieve(object)
+      if object.id && response = self.first(:stripe_id => object.id)
+        return response
+      else
+        response = self.new
+        object.each do |key, val|
+          next if val.nil?
+          key = :stripe_id if key == :id
+          self.set_key(key, val) if !self.column_names.include?(key.to_s)
+          response.send(:"#{key}=", Zebra::Util.convert_to_zebra_object(val))
+        end
+        response.save!
+        response
+      end
+    end
+
+    def self.set_key(key_name, value)
+      case value
+      when Stripe::ListObject
+        many key_name, :class_name => "Zebra::ZebraObject::#{value.first.class.class_name}"
+      when *Stripe::Util.object_classes.values
+        one key_name, :class_name => "Zebra::ZebraObject::#{value.class.class_name}"
+      when TrueClass, FalseClass
+        key key_name, Boolean
+      else
+        key key_name, value.class
+      end
+    end
   end
 
   class Harvester
@@ -22,7 +50,7 @@ module Zebra
     end
 
     def perform(opts={})
-      Zebra::Util.convert_to_zebra_object(self.class.stripe_class.all)
+      Zebra::Util.convert_to_zebra_object(self.class.stripe_class.all(opts))
     end
   end
 
@@ -35,7 +63,9 @@ module Zebra
         object.map { |o| convert_to_zebra_object(o) }
       when Stripe::ListObject
         convert_to_zebra_object(object.to_a)
-      when *(Stripe::Util.object_classes.values << Stripe::StripeObject)
+      when *Stripe::Util.object_classes.values
+        Zebra::ZebraObject.const_get(object.class.class_name).make_or_retrieve(object)
+      when Stripe::StripeObject
         convert_to_zebra_object(object.to_hash)
       else
         object
